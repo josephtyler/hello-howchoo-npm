bcecc0408e040714480c28e67e26db5fa2f50f14
Tyler <josephtylerjones@gmail.com>
2019-09-27 15:32:24 -0700

    Added files

---
+++ dist/hello-howchoo.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.helloHowchoo = void 0;
+
+var helloHowchoo = function helloHowchoo() {
+  console.log('Hello, Howchoo!!');
+};
+
+exports.helloHowchoo = helloHowchoo;
---
+++ src/hello-howchoo.js
@@ -0,0 +1,3 @@
+export const helloHowchoo = () => {
+   console.log('Hello, Howchoo!!');
+}
